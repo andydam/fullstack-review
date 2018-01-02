cb96d5e2f1654da17caf4139eeffb7e9933ea030
Andy Dam <andy@andydam.com>
2018-01-02 15:34:22 -0800

    completed basic post and get functionality

--- server/index.js
+++ server/index.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {getReposByUsername} = require('../helpers/github.js');
+const db = require('../database');
 let app = express();
+
+app.post('/repos', bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} on ${req.path}`);
+  if (req.body) {
+    console.log(`Data: ${JSON.stringify(req.body)}`);
+  }
+  next();
+});
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+app.post('/repos', (req, res) => {
+  getReposByUsername(req.body.username)
+    .then(data => {
+      if (data.message) {
+        return Promise.reject(err);
+      }
+      return Promise.all(data.map(repo => db.save(repo)));
+    })
+    .then(repoDocs => {
+      res.statusCode = 200;
+      res.end(JSON.stringify(repoDocs));
+    })
+    .catch(err => {
+      res.statusCode = 500;
+      res.end(JSON.stringify(err));
+    });
 });
 
-app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+app.get('/repos', (req, res) => {
+  db.get()
+    .then(repos => {
+      res.statusCode = 200;
+      res.end(JSON.stringify(repos));
+    })
+    .catch(err => {
+      res.statusCode = 500;
+      res.end(JSON.stringify(err));
+    });
 });
 
 let port = 1128;
